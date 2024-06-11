0fa5b0548aeffa25ada4169e2e8010acc41ecf81
duduzinmilgrau <eduardobr1612@gmail.com>
2024-06-10 23:05:52 -0300

    yan mama geral

--- InterCult/src/app/pages/content/content.component.ts
+++ InterCult/src/app/pages/content/content.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
-
+  imgcidade1: string =  'assets/vancouver1.jpg';
+  imgcidade2: string =  'assets/vancouver2.JPG';
+  imgcidade3: string =  'assets/vancouver3.jpg';
 }
